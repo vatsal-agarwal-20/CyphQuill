8dcee35cc357860d853b3ae2eaca1d1046d7e818
dis-coder0904 <80644981+vatsal-agarwal-20@users.noreply.github.com>
2023-12-20 01:49:39 +0530

    landing page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { cn } from '@/lib/utils'
 
+import "react-loading-skeleton/dist/skeleton.css"
+import Navbar from '@/components/Navbar'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -19,7 +22,10 @@ export default function RootLayout({
     <html lang="en" className='light'>
       <body className={cn(
         'min-h-screen font-sans antialiased grainy', inter.className
-      )}>{children}</body>
+      )}>
+        <Navbar/>
+        {children}
+      </body>
     </html>
   )
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,6 @@
-import clsx, {ClassValue} from "clsx"
-import {twMerge} from "tailwind-merge"
-
-export function cn(...inputs: ClassValue[]){
-    // twMerge: merge two types of styles
-    // eg: px-2 py-2 --> p-2 (lhs = rhs)
-    return twMerge(clsx(inputs))
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+ 
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
